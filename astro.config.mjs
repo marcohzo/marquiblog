f7556219fe4c0094e73c3d13b71e8ee83723566f
Marco Herazo <marcoherazo11@gmail.com>
2023-10-06 19:38:23 -0300

    Install preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({ site: "https://marquiblog.vercel.app/" });
+export default defineConfig({
+  site: "https://marquiblog.vercel.app/",
+  integrations: [preact()]
+});
