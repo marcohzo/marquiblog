043f29e83434d7b65fae862035d7355056a2c8f7
maticamisay-notebook <maticamisay99@hotmail.com>
2023-10-09 00:47:02 -0300

    add tailwind

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -8,8 +8,15 @@ export default function Greeting({ messages }) {
 
   return (
     <div>
-      <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>New Greeting</button>
+      <h3 class="font-bold text-3xl text-center text-gray-800">
+        {greeting}! Thank you for visiting!
+      </h3>
+      <button
+        onClick={() => setGreeting(randomMessage())}
+        class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        New Greeting
+      </button>
     </div>
   );
 }
