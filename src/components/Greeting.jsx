83916b7af575014e434fa240475d9eaaab3261bd
maticamisay-notebook <maticamisay99@hotmail.com>
2023-10-09 01:09:12 -0300

    update greeting styles

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -7,13 +7,13 @@ export default function Greeting({ messages }) {
   const [greeting, setGreeting] = useState(messages[0]);
 
   return (
-    <div>
+    <div class="flex flex-row items-center justify-between gap-4">
       <h3 class="font-bold text-3xl text-center text-gray-800">
         {greeting}! Thank you for visiting!
       </h3>
       <button
         onClick={() => setGreeting(randomMessage())}
-        class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
         New Greeting
       </button>
