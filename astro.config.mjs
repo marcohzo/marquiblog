31f9b4093568c32101fe704b8548c4ae29e50f16
Marco Herazo <marcoherazo11@gmail.com>
2023-10-11 23:23:17 -0300

    fix server adapter. Install node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   site: "https://marquiblog.vercel.app/",
   integrations: [preact(), tailwind()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
