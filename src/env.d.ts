d558402456872e9ea34720702fb344d750d50f16
Marco Herazo <marcoherazo11@gmail.com>
2023-10-13 20:25:44 -0300

    update firebase config

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,10 +6,14 @@ interface ImportMetaEnv {
     readonly FIREBASE_CLIENT_ID: string;
     readonly FIREBASE_AUTH_URI: string;
     readonly FIREBASE_TOKEN_URI: string;
-    readonly FIREBASE_AUTH_CERT_URL: string
-    readonly FIREBASE_CLIENT_CERT_URL: string
-    readonly FIREBASE_MESSAGING_ID: string
+    readonly FIREBASE_AUTH_CERT_URL: string;
+    readonly FIREBASE_CLIENT_CERT_URL: string;
+    readonly FIREBASE_API_KEY: string;
+    readonly FIREBASE_AUTH_DOMAIN: string;
     readonly FIREBASE_STORAGE_BUCKET: string;
+    readonly FIREBASE_MESSAGING_SENDER_ID: string;
+    readonly FIREBASE_APP_ID: string;
+    readonly FIREBASE_MEASUREMENT_ID: string;
   }
 
   interface ImportMeta {
--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -1,12 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_PRIVATE_KEY,
-  authDomain: import.meta.env.FIREBASE_AUTH_URI,
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_ID,
-  appId: import.meta.env.FIREBASE_PRIVATE_KEY_ID,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
 };
 
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
