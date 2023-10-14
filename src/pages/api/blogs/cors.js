cc23195fb06282affcff42aeab8f7f459f586b56
Marco Herazo <marcoherazo11@gmail.com>
2023-10-14 02:40:22 -0300

    Update cors.js

--- src/pages/api/blogs/cors.js
+++ src/pages/api/blogs/cors.js
@@ -1,4 +1,4 @@
-module.exports = (req, res, next) => {
+export default (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
