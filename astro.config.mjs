e669a9e077ae93af41750d736cb4cf8f09c46032
Marco Herazo <marcoherazo11@gmail.com>
2023-10-13 23:22:48 -0300

    update vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
-
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://marquiblog.vercel.app/",
   integrations: [preact(), tailwind()],
   output: "hybrid",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
 });
